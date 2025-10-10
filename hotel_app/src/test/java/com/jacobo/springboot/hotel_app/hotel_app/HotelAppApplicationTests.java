3d8e5943b3bc6f73346aed55f9a473005c83da57
J. cob <55332149+JacoboOsorio@users.noreply.github.com>
2025-10-10 14:59:15 -0500

    Creating files for the hotel project

---
+++ hotel_app/src/test/java/com/jacobo/springboot/hotel_app/hotel_app/HotelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacobo.springboot.hotel_app.hotel_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
