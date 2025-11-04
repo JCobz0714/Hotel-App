6398aa06a1d390c69c7ee8712586f86a0c8e76f8
J. cob <jacoboosorio299589@gmail.com>
2025-11-04 14:52:19 -0500

    Fixing minor erros

--- hotel_app/src/main/java/com/jacobo/springboot/hotel_app/hotel_app/entities/Room.java
+++ hotel_app/src/main/java/com/jacobo/springboot/hotel_app/hotel_app/entities/Room.java
@@ -1,8 +1,5 @@
 package com.jacobo.springboot.hotel_app.hotel_app.entities;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
