47290dc9e4877f209605a870a46cfbb3977472bb
J. cob <jacoboosorio299589@gmail.com>
2026-01-27 18:32:57 -0500

    Creating BookingDTO file for handling user's HTTP requests

--- hotel_app/src/main/java/com/jacobo/springboot/hotel_app/hotel_app/dto/BookingDTO.java
+++ hotel_app/src/main/java/com/jacobo/springboot/hotel_app/hotel_app/dto/BookingDTO.java
@@ -1,5 +1,54 @@
 package com.jacobo.springboot.hotel_app.hotel_app.dto;
 
-public class BookingDTO {
+import java.sql.Date;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class BookingDTO {
+    @NotNull
+    private Date checkInDate;
+
+    @NotNull
+    private Date checkOutDate;
+
+    @NotNull
+    private Long roomNumber;
+
+    @NotBlank
+    private String guestName;
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public Long getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Long roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    
 }
