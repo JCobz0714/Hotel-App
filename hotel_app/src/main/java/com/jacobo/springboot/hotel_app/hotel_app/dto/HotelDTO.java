b1ef5b8bffbc9b6eec0b8b25afabd4369be86c60
J. cob <jacoboosorio299589@gmail.com>
2026-01-27 18:38:09 -0500

    Creating HotelDTO for handling user's HTTP requests

--- hotel_app/src/main/java/com/jacobo/springboot/hotel_app/hotel_app/dto/HotelDTO.java
+++ hotel_app/src/main/java/com/jacobo/springboot/hotel_app/hotel_app/dto/HotelDTO.java
@@ -1,5 +1,23 @@
 package com.jacobo.springboot.hotel_app.hotel_app.dto;
 
 public class HotelDTO {
+    private String hotelName;
 
+    private Long roomNumber;
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public Long getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Long roomNumber) {
+        this.roomNumber = roomNumber;
+    }
 }
