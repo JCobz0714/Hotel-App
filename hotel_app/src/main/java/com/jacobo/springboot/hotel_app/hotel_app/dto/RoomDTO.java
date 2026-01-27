158d53f0186bba92ab41f8f155cebfc1b39afa8b
J. cob <jacoboosorio299589@gmail.com>
2026-01-27 18:41:00 -0500

    Creating RoomDTO file for handling user's HTTP requests

--- hotel_app/src/main/java/com/jacobo/springboot/hotel_app/hotel_app/dto/RoomDTO.java
+++ hotel_app/src/main/java/com/jacobo/springboot/hotel_app/hotel_app/dto/RoomDTO.java
@@ -1,5 +1,39 @@
 package com.jacobo.springboot.hotel_app.hotel_app.dto;
 
-public class RoomDTO {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
+public class RoomDTO {
+    @NotNull
+    private Long roomNumber;
+
+    @NotBlank
+    private String hotelName;
+
+    @NotNull
+    private Long bookingId;
+
+    public Long getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Long roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public Long getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(Long bookingId) {
+        this.bookingId = bookingId;
+    }
 }
